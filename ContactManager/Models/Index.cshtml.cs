6948515fb1d379f26426fc823fe0d2e76db6280b
romilrey <200528076@student.georgianc.on.ca>
2022-08-09 00:07:12 -0400

    bdat_1001_final_project_v1

---
+++ ContactManager/Models/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContactManager.Data;
+
+namespace ContactManager.Models
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContactManager.Data.ApplicationDbContext _context;
+
+        public IndexModel(ContactManager.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Contact> Contact { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Contact != null)
+            {
+                Contact = await _context.Contact.ToListAsync();
+            }
+        }
+    }
+}
---
+++ ContactManager/Pages/Privacy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ContactManager.Pages
+{
+    [AllowAnonymous]
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
